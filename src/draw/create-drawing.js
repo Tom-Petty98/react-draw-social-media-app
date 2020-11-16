ab3e3f737df2bdf1823ca9b3fbc1e43b8d84c405
Tom Petty <Tom@javascript.europe-west2-c.c.upbeat-airway-275120.internal>
2020-11-16 15:59:52 +0000

    Added draw functionality

--- src/draw/create-drawing.js
+++ src/draw/create-drawing.js
@@ -1,5 +1,10 @@
-import p5 from "p5";
-import p5  from p5;
+import p5  from "p5";
+
+const colorInput = document.getElementById('color');
+const weight = document.getElementById('weight');
+const clear = document.getElementById('clear');
+const paths = [];
+let currentPath = [];
 
 function setup() {
   // create a canvas which is full width and height
@@ -9,11 +14,48 @@ function setup() {
   background(255);
 }
 
-function draw() {}
+function draw() {
+      // disabled filling geometry - p5js function
+      noFill();
+
+      if (mouseIsPressed) {
+          // Store the location of the mouse
+          const point = {
+              x: mouseX,
+              y: mouseY,
+              color: colorInput.value,
+              weight: weight.value
+          };
+          currentPath.p5.push(point);
+        }
+
+      // Looping over all the paths and drawing all the points inside them
+      paths.forEach(path => {
+        beginShape();
+        path.forEach(point => {
+            stroke(point.color);
+            strokeWeight(point.weight);
+            vertex(point.x, point.y);
+        });
+      endShape();
+      });
+}
+
+//saves prev shape and cleans up path for new shape
+function mousePressed() {
+  currentPath = [];
+  paths.p5.push(currentPath);
+}
+
+clear.addEventListener('click', () => {
+  paths.splice(0);
+  background(255);
+});
 
 function DrawPage() {
   return (
     <div class="sidebar">
+    <script defer src='../src/draw/create-drawing.js'></script>
     <ul>
         <li>
             <label for="color">Color:</label>
